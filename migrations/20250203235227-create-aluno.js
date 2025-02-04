ea7fb8bb243c714bd79d9540d400f9d33db5d307
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-02-04 19:54:33 -0300

    adjustes migration

--- migrations/20250203235227-create-aluno.js
+++ migrations/20250203235227-create-aluno.js
@@ -3,23 +3,24 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Alunos', {
-      id: {
+      matricula: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      matricula: {
-        type: Sequelize.INTEGER
-      },
       nome: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
       },
       curso: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
