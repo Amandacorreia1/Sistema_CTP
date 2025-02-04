b91a8fa660407e35940e0ede71232e9f9665b2e9
Danielly Benício <daniellybeniciodearaujo@gmail.com>
2025-02-04 19:24:32 -0300

    adjustes migration

--- migrations/20250203232846-create-amparo-legal.js
+++ migrations/20250203232846-create-amparo-legal.js
@@ -2,19 +2,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('AmparoLegals', {
+    await queryInterface.createTable('AmparoLegal', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
-        type: Sequelize.INTEGER
-      },
       nome: {
-        type: Sequelize.STRING
-      },
+        type: Sequelize.ENUM('Receber educação de qualidade, que promova o seu desenvolvimento profissional e humano.',
+        'Solicitar à coordenadoria de seu curso orientação para solução de eventuais dificuldades na vida acadêmica.',
+        'Denunciar, tendo assegurado o anonimato, o mau uso do patrimônio público, depredações e atos de vandalismo, condutas ilícitas, por parte dos pares e servidores.',
+        'Outros'
+      )},
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('AmparoLegals');
+    await queryInterface.dropTable('AmparoLegal');
   }
 };
