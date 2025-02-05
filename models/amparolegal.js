dbdea5c50710b7d221c4fcc00dc0981496630c3d
Danielly Benício <daniellybeniciodearaujo@gmail.com>
2025-02-04 23:30:38 -0300

    adjustes

--- models/amparolegal.js
+++ models/amparolegal.js
@@ -5,29 +5,26 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class AmparoLegal extends Model {
     static associate(models) {
-      AmparoLegal.belongsToMany(models.Demanda, { through: 'AmparoDemanda', foreignKey: 'amparolegal_id', otherKey: 'demanda_id' });
+      AmparoLegal.belongsToMany(models.Demanda, { through: 'AmparoDemandas', foreignKey: 'amparolegal_id', otherKey: 'demanda_id' });
     }
   }
   AmparoLegal.init({
     id:{
       type: DataTypes.INTEGER,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
     nome: {
-      type: DataTypes.ENUM, 
-      values: [
-        'Receber educação de qualidade, que promova o seu desenvolvimento profissional e humano.',
-        'Solicitar à coordenadoria de seu curso orientação para solução de eventuais dificuldades na vida acadêmica.',
-        'Denunciar, tendo assegurado o anonimato, o mau uso do patrimônio público, depredações e atos de vandalismo, condutas ilícitas, por parte dos pares e servidores.',
-        'Outros'
-      ],
-
-      allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },    
   }, {
     sequelize,
     modelName: 'AmparoLegal',
+    tableName: 'AmparoLegal',
+    timestamps: true,
   });
   return AmparoLegal;
 };
