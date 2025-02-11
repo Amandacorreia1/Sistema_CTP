70f674e3aaa12cafa25f6dca314b60bb6a770d35
Danielly Benício <daniellybeniciodearaujo@gmail.com>
2025-02-10 21:00:01 -0300

    add atribute in referral

--- models/encaminhamento.js
+++ models/encaminhamento.js
@@ -34,6 +34,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
+    data:{
+      type: DataTypes.DATE, 
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Encaminhamento',
