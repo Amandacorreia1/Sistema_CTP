81c25301d27ac41c247068f42df61c01dbca2ebc
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-03-09 18:11:07 -0300

    Adjustes demanda in app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/authRoutes.js';
 import condicaoRoutes from './routes/condicoesRoutes.js';
 import amparolegalRoutes from './routes/amparoLegalRoutes.js';
 import encaminhamentoRoutes from './routes/encaminhamentoRoutes.js';
+import demandaRoutes from './routes/demandaRoutes.js';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', condicaoRoutes); 
 app.use('/api', amparolegalRoutes);
-app.use('/api',encaminhamentoRoutes);
+app.use('/api', encaminhamentoRoutes);
+app.use('/api', demandaRoutes);
 
 export default app;
