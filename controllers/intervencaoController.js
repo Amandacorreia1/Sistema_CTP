bd717b2e09ae2decb1af4fed0350fdb3c50294b4
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-03-10 22:14:20 -0300

    adjustes in intervencaoController and router

--- controllers/intervencaoController.js
+++ controllers/intervencaoController.js
@@ -3,12 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Criar Intervenção
 export const criarIntervencao = async (req, res) => {
-    const { descricao } = req.body;  // Como você mencionou que só precisa da descrição
+    const { descricao } = req.body;  
 
     try {
-        // Criando a nova intervenção
         const novaIntervencao = await db.Intervencao.create({
             descricao,
         });
@@ -23,10 +21,8 @@ export const criarIntervencao = async (req, res) => {
     }
 };
 
-// Listar todas as Intervenções
 export const listarIntervencao = async (req, res) => {
     try {
-        // Buscando todas as intervenções
         const intervencao_res = await db.Intervencao.findAll();
 
         return res.status(200).json({
@@ -39,28 +35,3 @@ export const listarIntervencao = async (req, res) => {
     }
 };
 
-// Listar Intervenções por Demanda
-export const listarIntervencaoPorDemanda = async (req, res) => {
-    const { demanda_id } = req.params;
-
-    try {
-        // Verificando se a demanda existe
-        const demanda_res = await db.Demanda.findByPk(demanda_id);
-        if (!demanda_res) {
-            return res.status(404).json({ error: 'Demanda não encontrada' });
-        }
-
-        // Buscando as intervenções associadas à demanda
-        const intervencao_res = await db.Intervencao.findAll({
-            where: { demanda_id },
-        });
-
-        return res.status(200).json({
-            intervencao: intervencao_res,
-            mensagem: "Intervenções da demanda carregadas com sucesso!",
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao listar as intervenções da demanda' });
-    }
-};
