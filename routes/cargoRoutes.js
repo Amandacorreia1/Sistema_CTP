6dfb4c449a7773132f66da8dda3bf62bff281b06
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-03-16 18:06:11 -0300

    add middlleware in cargo

--- routes/cargoRoutes.js
+++ routes/cargoRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {listarCargos, listarCargoPorId,cadastrarCargo} from '../controllers/cargoController.js';
+import { autenticarToken} from '../middlewares/authMiddlware.js';
 
 const router = express.Router();
 
-router.get('/cargos',listarCargos);
-router.get('/cargos/:id',listarCargoPorId);
-router.post('/cargos',cadastrarCargo);
+router.get('/cargos',autenticarToken, listarCargos);
+router.get('/cargos/:id', autenticarToken, listarCargoPorId);
+router.post('/cargos',autenticarToken, cadastrarCargo);
 
 export default router;
