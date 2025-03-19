fb78a6dd4506e6e93ae946248b842d41ff6fc3dd
Danielly Benicio <daniellybeniciodearaujo@gmail.com>
2025-03-19 17:29:46 -0300

    add route of merge cargo-name

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { buscarTodosUsuarios, buscarUsuariosPorId, buscarUsuarioPorNome, editarUsuario, excluirUsuario, usuariosPorCargo } from '../controllers/usuarioController.js';
+import { buscarTodosUsuarios, buscarUsuariosPorId, buscarUsuarioPorNome, editarUsuario, excluirUsuario, usuariosPorCargo, UsuariosPorNomeOuCargo } from '../controllers/usuarioController.js';
 import { autenticarToken, authorizeRole } from '../middlewares/authMiddlware.js';
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.get('/usuarios/:nome', autenticarToken, authorizeRole('Admin'), buscarUsu
 router.get('/usuario/:id', autenticarToken, authorizeRole('Admin'), buscarUsuariosPorId);
 router.put('/usuario/:id', autenticarToken, authorizeRole('Admin'), editarUsuario);
 router.delete('/usuario/:id', autenticarToken, authorizeRole('Admin'), excluirUsuario);
+router.get('/usuarios/busca/:termo', autenticarToken, authorizeRole('Admin'), UsuariosPorNomeOuCargo);
 
 export default router;
 
