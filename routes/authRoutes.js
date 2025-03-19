0b4a6fd81a442c8005fab6c067ec8bd4a3698b8e
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-03-19 17:53:38 -0300

    adjuste route of cadastro

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { cadastrarUsuario, login, recuperarSenha, redefinirSenha } from '../controllers/authController.js';
+import { autenticarToken, authorizeRole } from '../middlewares/authMiddlware.js'; 
 
 const router = express.Router();
 
-router.post('/cadastro', cadastrarUsuario);
+router.post('/cadastro', autenticarToken, authorizeRole('Admin'), cadastrarUsuario);
 router.post('/login', login);
 router.post('/recuperar-senha', recuperarSenha);
 router.post('/redefinir-senha', redefinirSenha)
