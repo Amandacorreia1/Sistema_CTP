16ef51bfbdd778d4c6e4039f94b8bec7597fcb45
Danielly Benicio <daniellybeniciodearaujo@gmail.com>
2025-03-22 18:40:17 -0300

    create route for mydemands and adjustes in router for list users

--- routes/demandaRoutes.js
+++ routes/demandaRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { criarDemanda, listarDemandas } from "../controllers/demandaController.js";
+import { criarDemanda, listarDemandas, listarNiveis, listarDemandasUsuario } from "../controllers/demandaController.js";
 import { autenticarToken } from '../middlewares/authMiddlware.js';
 import { restringirAdmin } from '../middlewares/restringirAdmin.js';
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.get('/demanda', autenticarToken, restringirAdmin(), listarDemandas);
 router.post('/demanda', autenticarToken, restringirAdmin(), criarDemanda);
+router.get('/demandaniveis', autenticarToken, restringirAdmin(), listarNiveis);
+router.get('/minhas-demandas', autenticarToken, listarDemandasUsuario);
 
 export default router;
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -4,7 +4,7 @@ import { autenticarToken, authorizeRole } from '../middlewares/authMiddlware.js'
 
 const router = express.Router();
 
-router.get('/usuarios', autenticarToken, authorizeRole('Admin'), buscarTodosUsuarios);
+router.get('/usuarios', autenticarToken, buscarTodosUsuarios);
 router.get('/usuarios/cargo/:cargo', autenticarToken, authorizeRole('Admin'), usuariosPorCargo);
 router.get('/usuarios/:nome', autenticarToken, authorizeRole('Admin'), buscarUsuarioPorNome);
 router.get('/usuario/:id', autenticarToken, authorizeRole('Admin'), buscarUsuariosPorId);
