fb7d77e99fe113df15274740cfc39ba12f656fd1
Amandacorreia1 <amandanascimentocorreia18@gmail.com>
2025-04-06 12:13:20 -0300

    Adjustes in seeders

--- seeders/20250314205349-demanda.cjs
+++ seeders/20250314205349-demanda.cjs
@@ -8,7 +8,7 @@ module.exports = {
     await queryInterface.bulkInsert('Demandas', [
       {descricao: 'Adaptação curricular para aluno com deficiência visual.', status: true, usuario_id: 1, createdAt: now, updatedAt: now},
       {descricao: 'Pedido de atendimento especializado para aluno com TDAH.', status: true, disciplina: "Estrutura de Dados", usuario_id: 2, createdAt: now, updatedAt: now},
-      {descricao: 'Aluno brigou em sala de aula', status: false, usuario_id: 3, createdAt: now, updatedAt: now}
+      {descricao: 'Aluno brigou em sala de aula', status: false, usuario_id: 4, createdAt: now, updatedAt: now}
     ]);
   },
   async down(queryInterface, Sequelize) {
--- seeders/20250314211935-aluno.cjs
+++ seeders/20250314211935-aluno.cjs
@@ -6,10 +6,9 @@ module.exports = {
     const now = new Date();
 
     await queryInterface.bulkInsert('Alunos', [
-      { matricula: 123456, nome: 'Amanda Correia', email: 'amanda.c@email.com', curso_id: 1, createdAt: now, updatedAt: now },
-      { matricula: 123457, nome: 'André Casimiro', email: 'andrec@email.com', curso_id: 2, createdAt: now, updatedAt: now },
-      { matricula: 123458, nome: 'Daniel Teixeira', email: 'daniel34@email.com', curso_id: 3, createdAt: now, updatedAt: now },
-      { matricula: 123459, nome: 'Danielly Benicio', email: 'daniellybenicio@email.com', curso_id: 4, createdAt: now, updatedAt: now }
+      { matricula: 20221035000028, nome: 'AMANDA DO NASCIMENTO CORREIA', email: 'amandanascimentocorreia18@gmail.com', curso_id: 6, createdAt: now, updatedAt: now },
+      { matricula: 20221035000281, nome: 'MARIA DANIELLY BENICIO DE ARAUJO', email: 'daniellybenicio@outlook.com', curso_id: 6, createdAt: now, updatedAt: now },
+      { matricula: 20221035000052, nome: 'DANIEL TEIXEIRA DA SILVA', email: 'daniel.teixeira08@aluno.ifce.edu.br', curso_id: 3, createdAt: now, updatedAt: now },
     ]);
   },
 
